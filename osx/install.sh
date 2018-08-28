2010db261848703fede4bc1c4315dfe33ded8bd3
Carlos Lalimarmo <clalimarmo@gmail.com>
2018-08-28 07:34:02 -0400

    fix .tmux.conf, add git completion

--- osx/install.sh
+++ osx/install.sh
@@ -2,3 +2,4 @@
 ln -sfn `pwd` $HOME/osx_dotfiles
 ln -sf $HOME/osx_dotfiles/.bash_profile $HOME/.bash_profile
 ln -sf $HOME/osx_dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/osx_dotfiles/git-completion.bash $HOME/git-completion.bash
